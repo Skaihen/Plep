ae8a98457fb824f3f2f3c324a3a9f31b50ed8b28
Skaihen <92516247+Skaihen@users.noreply.github.com>
2022-06-25 17:27:28 +0000

    tokens

--- src/plep.cpp
+++ src/plep.cpp
@@ -1,4 +1,4 @@
-#include <string>;
+#include <string>
 
 // The lexer returns tokens [0-255] if it is an unknown character, otherwise one
 // of these for known things.
@@ -17,3 +17,49 @@ enum Token
 
 static std::string IdentifierStr; // Filled in if tok_identifier
 static double NumVal;             // Filled in if tok_number
+
+// gettok - Return the next token from standard input.
+static int gettok()
+{
+    static int LastChar = ' ';
+
+    // Skip any whitespace.
+    while (isspace(LastChar))
+    {
+        LastChar = getchar();
+    }
+
+    if (isalpha(LastChar))
+    { // identifier: [a-zA-Z][a-zA-Z0-9]*
+        IdentifierStr = LastChar;
+        while (isalnum((LastChar = getchar())))
+        {
+            IdentifierStr += LastChar;
+        }
+
+        if (IdentifierStr == "def")
+        {
+            return tok_def;
+        }
+
+        if (IdentifierStr == "extern")
+        {
+            return tok_extern;
+        }
+
+        return tok_identifier;
+    }
+
+    if (isdigit(LastChar) || LastChar == '.')
+    { // Number: [0-9.]+
+        std::string NumStr;
+        do
+        {
+            NumStr += LastChar;
+            LastChar = getchar();
+        } while (isdigit(LastChar) || LastChar == '.');
+
+        NumVal = strtod(NumStr.c_str(), 0);
+        return tok_number;
+    }
+}
