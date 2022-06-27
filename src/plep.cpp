97b9a220f40fc411b446e60a2ef072e48438fafb
Skaihen <92516247+Skaihen@users.noreply.github.com>
2022-06-27 20:34:36 +0000

    ast

--- src/plep.cpp
+++ src/plep.cpp
@@ -147,3 +147,25 @@ class CallExprAST : public ExprAST
 public:
     CallExprAST(const std::string &Callee, std::vector<std::unique_ptr<ExprAST>> Args) : Callee(Callee), Args(std::move(Args)) {}
 };
+
+// PrototypeAST - This class represents the "prototype" for a function, which captures its name, and its argument names (thus implicitly the number of arguments the function takes).
+class PrototypeAST
+{
+    std::string Name;
+    std::vector<std::string> Args;
+
+public:
+    PrototypeAST(const std::string &name, std::vector<std::string> Args) : Name(name), Args(std::move(Args)) {}
+
+    const std::string &getName() const { return Name; }
+};
+
+// FunctionAST - This class represents a function definition itself.
+class FunctionAST
+{
+    std::unique_ptr<PrototypeAST> Proto;
+    std::unique_ptr<ExprAST> Body;
+
+public:
+    FunctionAST(std::unique_ptr<PrototypeAST> Proto, std::unique_ptr<ExprAST> Body) : Proto(std::move(Proto)), Body(std::move(Body)) {}
+};
