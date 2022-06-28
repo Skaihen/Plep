24301fe755925e20530b22fd0f91ca16e03134b7
Skaihen <92516247+Skaihen@users.noreply.github.com>
2022-06-28 21:32:56 +0000

    parser

--- src/plep.cpp
+++ src/plep.cpp
@@ -169,3 +169,104 @@ class FunctionAST
 public:
     FunctionAST(std::unique_ptr<PrototypeAST> Proto, std::unique_ptr<ExprAST> Body) : Proto(std::move(Proto)), Body(std::move(Body)) {}
 };
+
+// CurTok - Provide a simple token buffer. CurTok is the current token the parser is looking at.
+static int CurTok;
+
+// getNextToken - Reads another token from the lexer and updates CurTok with its results.
+static int getNextToken()
+{
+    return CurTok = gettok();
+}
+
+// LogError* - These are little helper functions for error handling.
+std::unique_ptr<ExprAST> LogError(const char *Str)
+{
+    fprintf(stderr, "LogError: %s\n", Str);
+    return nullptr;
+}
+
+std::unique_ptr<PrototypeAST> LogErrorP(const char *Str)
+{
+    LogError(Str);
+    return nullptr;
+}
+
+// numberexpr ::= number
+static std::unique_ptr<ExprAST> ParseNumberExpr()
+{
+    auto Result = std::make_unique<NumberExprAST>(NumVal);
+    getNextToken(); // consume the number
+    return std::move(Result);
+}
+
+// parenexpr ::= '(' expression ')'
+static std::unique_ptr<ExprAST> ParseParenExpr()
+{
+    getNextToken(); // eat (
+    auto V = ParseExpression();
+
+    if (!V)
+    {
+        return nullptr;
+    }
+
+    if (CurTok == ')')
+    {
+        getNextToken(); // eat )
+        return V;
+    }
+    else
+    {
+        return LogError("expected ')'");
+    }
+}
+
+// identifierexpr
+//   ::= identifier
+//   ::= identifier '(' expression* ')'
+static std::unique_ptr<ExprAST> ParseIdentifierExpr()
+{
+    std::string IdName = IdentifierStr;
+
+    getNextToken(); // eat identifier.
+
+    if (CurTok == '(')
+    {
+        // Call.
+        getNextToken(); // eat (
+        std::vector<std::unique_ptr<ExprAST>> Args;
+        if (CurTok != ')')
+        {
+            while (1)
+            {
+                if (auto Arg = ParseExpression())
+                {
+                    Args.push_back(std::move(Arg));
+                }
+                else
+                {
+                    return nullptr;
+                }
+                if (CurTok == ')')
+                {
+                    break;
+                }
+                if (CurTok != ',')
+                {
+                    return LogError("Expected ')' or ',' in argument list");
+                }
+                getNextToken();
+            }
+        }
+    }
+    else
+    { // Simple variable ref.
+        return std::make_unique<VariableExprAST>(IdName);
+    }
+
+    // Eat the ')'.
+    getNextToken();
+
+    return std::make_unique<CallExprAST>(IdName, std::move(Args));
+}
