4ccff851ac548dc404a41123540c055fd2d2d6e3
Skaihen <92516247+Skaihen@users.noreply.github.com>
2022-10-16 20:17:46 +0000

    init

---
+++ plep.py
@@ -0,0 +1,17 @@
+TT_INT = 'TT_INT'
+TT_FLOAT = 'FLOAT'
+TT_PLUS = 'PLUS'
+TT_MINUS = 'MINUS'
+TT_MUL = 'MUL'
+TT_DIV = 'DIV'
+
+
+class Token:
+    def __init__(self, type_, value):
+        self.type = type_
+        self.value = value
+
+    def __repr__(self):
+        if self.value:
+            return f'{self.type}:{self.value}'
+        return f'{self.type}'
