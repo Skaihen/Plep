61184c20a148eec88cce5131545d7234c5e1e5e9
Skaihen <92516247+Skaihen@users.noreply.github.com>
2022-10-17 17:46:20 +0000

    need to refact

--- shell.py
+++ shell.py
@@ -1,9 +1,14 @@
-from unittest import result
 import plep
 
 while True:
-    text = input('plep> ')
-    result, error = plep.run('<stdin>', text)
+	text = input('plep> ')
+	if text.strip() == "": continue
+	result, error = plep.run('<stdin>', text)
 
-    if error: print(error.as_string())
-    else: print(result)
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
